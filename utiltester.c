24d02c9cdaf5f40c98fa65a9517fe4f6c009fadf
urj2258 <liaqtumer296@gmail.com>
2023-12-21 13:35:59 +0500

    making main function and printf

--- utiltester.c
+++ utiltester.c
@@ -1,3 +1,11 @@
 #include"util.h"
 #include<stdio.h>
 
+int main()
+{
+    double a=45.0;
+
+     printf("Testing degrees to radians:");
+    printf("\nInput:%f degrees\n",a);
+      printf("expected output:%f radians\n",degreesToRadians(a));
+    printf("Actual output:%f radians\n",degreesToRadians(a));
