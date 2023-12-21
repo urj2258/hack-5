e9e69d6f7bc0dea46f503f1481d19dcee49be871
urj2258 <liaqtumer296@gmail.com>
2023-12-21 13:40:33 +0500

    printing for test air distance

--- utiltester.c
+++ utiltester.c
@@ -17,3 +17,11 @@ int main()
     else{
         printf("test failed!");
     }
+
+    double lat1=37.7749,lon1=-122.4194,lat2=34.0522,lon2=-118.2437;
+     printf("testing Air Distance:");
+
+    printf("input:(%f,%f) to (%f,%f)\n",lat1, lon1, lat2, lon2);
+    printf("Expected Output: %f km\n", getAirDistance(lat1, lon1, lat2, lon2));
+    printf("Actual Output: %f km\n", getAirDistance(lat1, lon1, lat2, lon2));
+    
